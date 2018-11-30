75ab961a37a9910184a66b1998788ce84d8e6383
Manuel Guilbault <manuel.guilbault@gmail.com>
2018-11-30 10:55:49 +0100

    Fix build tool.

--- tools/extract-version-number.ps1
+++ tools/extract-version-number.ps1
@@ -4,10 +4,11 @@ param(
 
 If ($BuilderNumber -match "^\d+\.\d+\.\d+\.\d+$") {
 	$version = $BuilderNumber
+	$buildRevision = $BuilderNumber.Split(".")[3]
 } Else {
 	$version = $BuilderNumber -replace "^.+\.(\d+)$", '1.0.0.$1'
+	$buildRevision = $BuilderNumber.Split('.')[-1]
 }
-$buildRevision = $BuilderNumber.Split(".")[3]
 Write-Host "##vso[task.setvariable variable=Version]$version"
 Write-Host "##vso[task.setvariable variable=BuildRevision]$buildRevision"
 Write-Host "Computed Version: $version"
