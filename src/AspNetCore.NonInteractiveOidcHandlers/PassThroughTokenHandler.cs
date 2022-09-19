a6553fe6505b172f9cddbde06e12fc5ab34b3330
Jorn Vanloofsvelt <6916250+JVanloofsvelt@users.noreply.github.com>
2022-09-19 09:27:32 +0200

    Update PassThroughTokenHandler.cs

--- src/AspNetCore.NonInteractiveOidcHandlers/PassThroughTokenHandler.cs
+++ src/AspNetCore.NonInteractiveOidcHandlers/PassThroughTokenHandler.cs
@@ -3,6 +3,7 @@ using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
+using IdentityModel.Client;
 
 namespace AspNetCore.NonInteractiveOidcHandlers
 {
