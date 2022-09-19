796201c204c5950b546b854a01e0a4370e37ce7c
Jorn Vanloofsvelt <6916250+JVanloofsvelt@users.noreply.github.com>
2022-09-19 09:21:49 +0200

    Create CachedTokenResponse.cs

---
+++ src/AspNetCore.NonInteractiveOidcHandlers/IdentityModel/Client/CachedTokenResponse.cs
@@ -0,0 +1,16 @@
+using System.Text.Json;
+
+namespace IdentityModel.Client;
+
+/// <summary>
+/// A token response that can be instantiated from a cached json document. The original TokenResponse class 
+/// only supported instantiation from a HttpResponseMessage.
+/// </summary>
+public class CachedTokenResponse : TokenResponse
+{
+    public CachedTokenResponse(string cachedJson)
+    {
+        Raw = cachedJson;
+        Json = JsonDocument.Parse(cachedJson).RootElement;
+    }
+}
